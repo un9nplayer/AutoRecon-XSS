4b7947a17fe1a145ece12b41c58c874b22cbcac2
Un9nPlayer <75741506+un9nplayer@users.noreply.github.com>
2023-06-28 00:21:36 +0800

    Update AutoRecon-XSS.sh

--- AutoRecon-XSS.sh
+++ AutoRecon-XSS.sh
@@ -19,18 +19,6 @@ print_color_text "\033[1;34m" "
                                     [Welcome to AutoRecon XSS!]
                                                   [@un9nplayer]
 "
-
-
-OUTPUT_DIR="ReconTarget"
-FINAL_URLS_FILE="${OUTPUT_DIR}/finalUrls.txt"
-JS_URLS_FILE="${OUTPUT_DIR}/JSurls.txt"
-VULNERABLE_URLS_FILE="${OUTPUT_DIR}/potentialVulnerableUrls.txt"
-XSS_VULNERABLE_URLS_FILE="${OUTPUT_DIR}/XSSvulnerableUrls.txt"
-SUBDOMAINS_FILE="${OUTPUT_DIR}/Subdomains.txt"
-ALIVE_DOMAINS_FILE="${OUTPUT_DIR}/AliveDomain.txt"
-XSSV_CONFIRM="${OUTPUT_DIR}/XSS.txt"
-XSSV_CONFIRM_URLS="${OUTPUT_DIR}/XSSV_CONFIRM_URLS.txt"
-
 #typing_animation
 typing_animation() {
   local text=$1
@@ -42,15 +30,15 @@ typing_animation() {
   echo
 }
 
-# Input target URL, starting year, and XSS payload
-target="$1"
-start_year="$2"
-xss_payload="$3"
-
 typing_animation "Welcome to the AutoRecon XSS." 0.01
 sleep 1
 typing_animation "Be patient, it's working fine! Please wait for a while until AutoRecon completes its job!" 0.01
 typing_animation "Checking the URL status!" 0.01
+
+# Input target URL, starting year, and XSS payload
+target="$1"
+start_year="$2"
+xss_payload="$3"
 
 
 # Check if the target URL is alive and returns status 200 OK using curl
@@ -65,6 +53,67 @@ if [[ "${status_code}" != "200" ]]; then
 fi
 
 print_color_text "\e[1;36m" "Target URL is alive with status 200 OK. Starting the reconnaissance process."
+sleep 1
+
+OUTPUT_DIR="ReconTarget"
+FINAL_URLS_FILE="${OUTPUT_DIR}/finalUrls.txt"
+JS_URLS_FILE="${OUTPUT_DIR}/JSurls.txt"
+VULNERABLE_URLS_FILE="${OUTPUT_DIR}/potentialVulnerableUrls.txt"
+XSS_VULNERABLE_URLS_FILE="${OUTPUT_DIR}/XSSvulnerableUrls.txt"
+SUBDOMAINS_FILE="${OUTPUT_DIR}/Subdomains.txt"
+ALIVE_DOMAINS_FILE="${OUTPUT_DIR}/AliveDomain.txt"
+XSSV_CONFIRM="${OUTPUT_DIR}/XSS.txt"
+XSSV_CONFIRM_URLS="${OUTPUT_DIR}/XSSV_CONFIRM_URLS.txt"
+
+# Check if the file VULNERABLE_URLS_FILE exists
+if [[ -f "${VULNERABLE_URLS_FILE}" ]]; then
+    read -p $'\e[1;36m'${VULNERABLE_URLS_FILE}' found! Do you want to directly check for XSS from the vulnerable URLs '${VULNERABLE_URLS_FILE}' file? (Y/N): ' check_xss_from_file
+
+    if [[ "$check_xss_from_file" == "Y" || "$check_xss_from_file" == "y" ]]; then
+        print_color_text "\e[1;36m" "Checking vulnerable URLs for XSS..."
+
+# Array to store processed URLs
+processed_urls=()
+
+    # Replace payload in URLs and filter vulnerable URLs
+    while IFS= read -r url; do
+      vulnerable_url=$(echo "${url}" | qsreplace "${xss_payload}")
+      if ! [[ "${processed_urls[*]}" =~ "${vulnerable_url}" ]]; then
+        processed_urls+=("${vulnerable_url}")
+        response=$(curl -s -L "${vulnerable_url}")
+        if [[ "${response}" == *"${xss_payload}"* ]]; then
+          echo -e "${url}" >> "${XSS_VULNERABLE_URLS_FILE}"
+          echo -e "\e[1;91m${url}\e[0m"
+        fi
+      fi
+    done < "${VULNERABLE_URLS_FILE}"
+
+    # Remove unwanted substrings from the file
+    grep -o 'http[s]\?://[^[:space:]]\+' "${XSS_VULNERABLE_URLS_FILE}" > "${XSSV_CONFIRM}"
+
+    # Function to perform DalFox scan on a URL
+    perform_dalfox_scan() {
+      local url="$1"
+      dalfox file "${url}" --silence --no-spinner --only-poc='v' -o "${XSSV_CONFIRM_URLS}"
+    }
+
+    # Run DalFox scan on vulnerable URLs
+    if [[ -f "${XSSV_CONFIRM}" ]]; then
+      print_color_text "\e[1;36m" "Running DalFox scan on vulnerable URLs..."
+      perform_dalfox_scan "${XSSV_CONFIRM}"
+      print_color_text "\e[1;36m" "DalFox scan completed."
+    fi
+
+    print_color_text "\e[1;33m" "Vulnerability check completed."
+
+    if [[ -s "${XSSV_CONFIRM_URLS}" ]]; then
+      print_color_text "\e[1;33m" "XSS Vulnerable URLs saved as ${XSSV_CONFIRM_URLS}."
+    else
+      print_color_text "\e[1;33m" "No XSS Vulnerable URLs found."
+    fi
+  fi
+fi
+
 
 # Create output directory if it doesn't exist
 mkdir -p "${OUTPUT_DIR}"
@@ -90,7 +139,7 @@ fi
 # Save alive domains from subdomains file
 if [[ -f "${SUBDOMAINS_FILE}" ]]; then
     print_color_text "\e[1;36m" "Finding alive domains from subdomains file."
-    httpx -l "${SUBDOMAINS_FILE}" -mc 200 -silent > "${ALIVE_DOMAINS_FILE}"
+    httpx -l "${SUBDOMAINS_FILE}" -mc 200,301,403,401,404 -silent > "${ALIVE_DOMAINS_FILE}"
     print_color_text "\e[1;36m" "Alive domains saved as ${ALIVE_DOMAINS_FILE}."
 fi
 
@@ -183,4 +232,5 @@ if [[ -s "${XSSV_CONFIRM_URLS}" ]]; then
     print_color_text "\e[1;33m" "XSS Vulnerable URLs saved as ${XSSV_CONFIRM_URLS}."
 else
     print_color_text "\e[1;33m" "No XSS Vulnerable URLs found."
+fi
 fi
